1aee64c04a1db967a9f38282ed46e162ea8ee62d
Anastasiya Kachanova <nastya.kachanova.94@gmail.com>
2022-08-08 18:16:54 +0300

    homework_16 the comments were fixed using switch...case

--- homework-16/calculator.ts
+++ homework-16/calculator.ts
@@ -1,25 +1,12 @@
-export function calculatorPlus(firstNumber: number, secondNumber: number) {
-    return firstNumber + secondNumber;
-}
-
-export function calculatorMinus(firstNumber: number, secondNumber: number) {
-    return firstNumber - secondNumber;
-}
-
-export function calculatorDivision(firstNumber: number, secondNumber: number) {
-    return firstNumber / secondNumber;
-}
-
-export class calculatorMultiplication{
-    firstNumber;
-    secondNumber;
-
-    constructor(firstNumber: number, secondNumber: number) {
-        this.firstNumber =  firstNumber;
-        this.secondNumber = secondNumber;
+export function calculator(firstNumber: number, secondNumber: number, operation: '+' | '-' | '/' | '*') {
+    switch(operation) {
+       case '+': 
+           return firstNumber + secondNumber;
+       case '-': 
+           return firstNumber - secondNumber;
+       case '/': 
+           return firstNumber / secondNumber;
+       case '*': 
+           return firstNumber * secondNumber;
     }
-
-    multiplication() {
-        return this.firstNumber * this.secondNumber;
-    }
-}
+} 
--- homework-16/custom.checker.ts
+++ homework-16/custom.checker.ts
@@ -1,24 +1,14 @@
-import {
-  calculatorPlus,
-  calculatorMinus,
-  calculatorDivision,
-  calculatorMultiplication,
-} from "./calculator";
-
+import {calculator} from "./calculator";
 
 
 export function customChecker(
   firstNumber: number,
   secondNumber: number,
   result: number,
-  sign: '+' | '-' | '/' | '*' )
+  operation: '+' | '-' | '/' | '*' )
 {
-  let res = new calculatorMultiplication(firstNumber, secondNumber);
-  if (
-    (sign === "+" && calculatorPlus(firstNumber, secondNumber) === result) ||
-    (sign === "-" && calculatorMinus(firstNumber, secondNumber) === result) ||
-    (sign === "/" && calculatorDivision(firstNumber, secondNumber) === result) ||
-    (sign === "*" && res.multiplication() === result)) {
+ 
+  if (calculator(firstNumber, secondNumber, operation) === result) {
     console.log("Test passed!");
   } else {
     console.log("Test failed!");
